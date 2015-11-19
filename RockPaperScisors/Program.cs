8a7b88c67ad450eecb760e150c29c840dc884217
Craig Hinrichs <craig.hinrichs@gmail.com>
2015-11-19 00:15:34 -0600

    Initial

---
+++ RockPaperScisors/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+
+
+
+class RockPaperScissors {
+
+	public static void Main (string[] args) {
+
+//		int ROCK = 0;
+//		int PAPER = 1;
+//		int SCISSORS = 2;
+
+		int DRAW = 0;
+		int LOSE = -1;
+		int WIN = 1;
+
+		int[,] winTable = new int[3,3] { 
+			{DRAW,LOSE,WIN},  // Element 0
+			{WIN,DRAW,LOSE},  // Element 1
+			{LOSE,WIN,DRAW}   // Element 2
+		}; 
+
+		int playerScore = 0;
+		int computerScore = 0;
+		int drawScore = 0;
+		Random rand = new Random ();
+
+		string answer = "";
+		do {
+			Console.Clear();
+			Console.WriteLine("-- Rock, Paper, Scissors --");
+
+			int computerChoice = rand.Next(0,3);
+
+			Console.WriteLine("Choose your weapon. Computer has already Choosen!\n\n");
+			Console.WriteLine("Player Score: " + playerScore + "  Computer Score: " + computerScore + " Draws: " + drawScore + "\n\n");
+			Console.Write("1] Rock\n2] Paper\n3] Scissors\n\n::-> ");
+
+			int playerChoice = Convert.ToInt32(Console.ReadLine()) - 1;
+
+			if(winTable[playerChoice,computerChoice] == DRAW) {
+				Console.WriteLine("It's a Draw!");
+				drawScore++;
+			} else if(winTable[playerChoice,computerChoice] == LOSE) {
+				Console.WriteLine("You LOSE!");
+				computerScore++;
+			} else if(winTable[playerChoice,computerChoice] == WIN) {
+				Console.WriteLine("You WIN!");
+				playerScore++;
+			}
+			Console.WriteLine("Would you like to play again? [y/n]  ");
+			answer = Console.ReadLine();
+
+		} while(answer.ToLower() == "yes" || answer.ToLower() == "y");
+	}
+}
---
+++ RockPaperScisors/ProgramSave.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+
+class RockPaperScissors {
+
+	public static void Main (string[] args) {
+
+		int ROCK = 0;
+		int PAPER = 1;
+		int SCISSORS = 2;
+
+		int DRAW = 0;
+		int LOSE = -1;
+		int WIN = 1;
+
+
+		int[,] winTable = new int[3,3];
+
+		winTable [ROCK, ROCK] = DRAW;
+		winTable [ROCK, PAPER] = LOSE;
+		winTable [ROCK, SCISSORS] = WIN;
+
+		int playerChoice = ROCK;
+		int computerChoice = ROCK;
+
+		if(winTable[playerChoice,computerChoice] == DRAW) {
+			Console.WriteLine("It's a Draw");
+		} else if(winTable[playerChoice,computerChoice] == LOSE) {
+			Console.WriteLine("It's a LOSE");
+		} else if(winTable[playerChoice,computerChoice] == WIN) {
+			Console.WriteLine("It's a WIN");
+		}
+
+	}
+}
