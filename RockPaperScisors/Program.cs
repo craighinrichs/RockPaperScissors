b1a6dce2a0514850f5d4434e84730502fdecaf0f
Craig Hinrichs <craig.hinrichs@gmail.com>
2015-11-22 20:24:17 -0600

    RockPaperScirssorsVersion13

--- RockPaperScisors/Program.cs
+++ RockPaperScisors/Program.cs
@@ -2,49 +2,13 @@
 
 class RockPaperScissors {
 
-	static RPSView view = new RPSView();
+	
 
-	public static void Main (string[] args) {
+    public static void Main (string[] args) {
 
-		RPSModel.Instance.AddNewPlayer(RPSModel.Instance.LIVEPLAYER);
-		RPSModel.Instance.AddNewPlayer(RPSModel.Instance.COMPUTER_PLAYER,true);
+        RPSView view = new RPSView();
 
-		string answer = "";
-		do {
-
-			view.DisplayGreeting();
-
-			view.DisplayPlayerScores();
-
-			RPSModel.Instance.GetPlayer(RPSModel.Instance.COMPUTER_PLAYER).ChooseWeapon();
-
-			view.PrintWeaponChoice();
-
-			RPSModel.Instance.GetPlayer(RPSModel.Instance.LIVEPLAYER).ChooseWeapon();
-
-
-			Player playerOne = RPSModel.Instance.GetPlayer(RPSModel.Instance.LIVEPLAYER);
-			Player playerTwo = RPSModel.Instance.GetPlayer(RPSModel.Instance.COMPUTER_PLAYER);
-
-			if(playerOne == playerTwo) {
-				view.DisplayDraw();
-				RPSModel.Instance.DrawScore();
-			} else {
-				Player winner = playerOne * playerTwo;
-				if(winner.name.Equals(RPSModel.Instance.LIVEPLAYER)) {
-					view.DisplayWin();
-					RPSModel.Instance.GetPlayer(RPSModel.Instance.LIVEPLAYER).Scored();
-				} else if(winner.name.Equals(RPSModel.Instance.COMPUTER_PLAYER)) {
-					view.DisplayLose();
-					RPSModel.Instance.GetPlayer(RPSModel.Instance.COMPUTER_PLAYER).Scored();
-				}
-			}
-
-			view.QuestionPlayAgain();
-			answer = Console.ReadLine();
-
-			view.ClearScreen();
-
-		} while(answer.ToLower() == "y" || answer.ToLower() == "yes");
-	}
+        RPSController controller = new RPSController(view);
+        controller.StartGame();
+    }
 }
---
+++ RockPaperScisors/RPSController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+public class RPSController {
+
+    private RPSView view;
+
+    public RPSController (RPSView view) {
+        this.view = view;
+    }
+
+    public void StartGame() {
+        AddPlayers();
+        GameLoop();
+    }
+
+    public void AddPlayers() {
+        RPSModel.Instance.AddNewPlayer(RPSModel.Instance.LIVEPLAYER);
+        RPSModel.Instance.AddNewPlayer(RPSModel.Instance.COMPUTER_PLAYER,true);
+    }
+
+    public void GameLoop() {
+        string answer = "";
+        do {
+
+            view.DisplayGreeting();
+
+            view.DisplayPlayerScores();
+
+            RPSModel.Instance.GetPlayer(RPSModel.Instance.COMPUTER_PLAYER).ChooseWeapon();
+
+            view.PrintWeaponChoice();
+
+            RPSModel.Instance.GetPlayer(RPSModel.Instance.LIVEPLAYER).ChooseWeapon();
+
+
+            Player playerOne = RPSModel.Instance.GetPlayer(RPSModel.Instance.LIVEPLAYER);
+            Player playerTwo = RPSModel.Instance.GetPlayer(RPSModel.Instance.COMPUTER_PLAYER);
+
+            if(playerOne == playerTwo) {
+                view.DisplayDraw();
+                RPSModel.Instance.DrawScore();
+            } else {
+                Player winner = playerOne * playerTwo;
+                if(winner.name.Equals(RPSModel.Instance.LIVEPLAYER)) {
+                    view.DisplayWin();
+                    //RPSModel.Instance.GetPlayer(RPSModel.Instance.LIVEPLAYER).Scored();
+                } else if(winner.name.Equals(RPSModel.Instance.COMPUTER_PLAYER)) {
+                    view.DisplayLose();
+                    //RPSModel.Instance.GetPlayer(RPSModel.Instance.COMPUTER_PLAYER).Scored();
+                }
+                winner.Scored();
+            }
+
+            view.QuestionPlayAgain();
+            answer = Console.ReadLine();
+
+            view.ClearScreen();
+
+        } while(answer.ToLower() == "y" || answer.ToLower() == "yes");
+    }
+}
+
